f7472109b7a0c4d36d379b7b14b5fb987470130c
lucasmelojs <lucascunhamelo@gmail.com>
2024-05-07 08:47:24 -0300

    reactRouterDOM

--- Reviews/src/main.jsx
+++ Reviews/src/main.jsx
@@ -1,10 +1,40 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+//pages
+import ErrorPage from './routes/ErrorPage';
+import Home from './routes/Home';
+import Login from './routes/Login/index';
+import Register from './routes/Register/index';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: '/',
+        element: <Home />
+      },
+      {
+        path: '/Login',
+        element: <Login />
+      },
+      {
+        path: '/Register',
+        element: <Register />
+      }
+
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <RouterProvider router={router} />
+  </React.StrictMode>
 )
